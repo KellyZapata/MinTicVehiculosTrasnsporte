ffaaa17c89e4b2e811cab963a1c92a9f72169fc8
Kelly Zapata <k.zapata1@utp.edu.co>
2022-09-02 15:48:08 -0500

    First commit

---
+++ VehiculosTransporte.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace VehiculosTransporte.App.Dominio
+{
+
+
+public class Persona{
+    public int Id {get;set;}
+    public string Nombres {get;set;}
+    public string Apellidos {get;set;}
+    public string Cedula {get;set;}
+    public date Fecha_Nacimiento {get;set;}
+    public string Numero_Telefono {get;set;}
+    public string Direccion {get;set;}
+}
+
+}
