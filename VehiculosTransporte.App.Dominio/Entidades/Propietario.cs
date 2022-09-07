5baf23e3466e93e0c1ebe3e2c1ae81fe916d3f67
Henrry Pulgarin <xteams.hp@gmail.com>
2022-09-06 20:41:38 -0500

    fix propietario

--- VehiculosTransporte.App.Dominio/Entidades/Propietario.cs
+++ VehiculosTransporte.App.Dominio/Entidades/Propietario.cs
@@ -5,8 +5,8 @@ namespace VehiculosTransporte.App.Dominio
 {
 
 public class Propietario: Persona{
-}
     [ForeignKey("Vehiculo")]
     public virtual int VehiculoId {get;set;}
     public virtual Vehiculo Vehiculo {get;set;}
+}
 }
