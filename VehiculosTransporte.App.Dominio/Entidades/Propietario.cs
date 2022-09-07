1d699a9b34cc06e83e51ba925d2b9aa74c4643c3
Henrry Pulgarin <xteams.hp@gmail.com>
2022-09-06 20:03:57 -0500

    conductor

--- VehiculosTransporte.App.Dominio/Entidades/Propietario.cs
+++ VehiculosTransporte.App.Dominio/Entidades/Propietario.cs
@@ -4,8 +4,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace VehiculosTransporte.App.Dominio
 {
 
-public class Propietario: Persona{
-}
-    [ForeignKey("Vehiculo")]
-    public virtual int VehiculoId {get;set;}
+    public class Propietario: Persona{
+
+        [ForeignKey("Vehiculo")]
+        public virtual int VehiculoId {get;set;}
+        public virtual Vehiculo Vehiculo {get;set;}
+    }
 }
