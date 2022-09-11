d61bd0b749b83057b49037f9fd4a4763b68d9aa9
Henrry Pulgarin <xteams.hp@gmail.com>
2022-09-11 11:30:08 -0500

    DetallesVehiculo

--- VehiculosTransporte.App.Frontend/Pages/Vehiculos/ListarVehiculo.cshtml.cs
+++ VehiculosTransporte.App.Frontend/Pages/Vehiculos/ListarVehiculo.cshtml.cs
@@ -6,15 +6,17 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using VehiculosTransporte.App.Persistencia;
 using VehiculosTransporte.App.Dominio;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+
 
 namespace VehiculosTransporte.App.Frontend.Pages
 {
-    public class CrearVehiculosModel : PageModel
+    public class ListarVehiculo : PageModel
     {
         private readonly IRepositorioVehiculos repositorioVehiculos;
         public IEnumerable<Vehiculo> vehiculos {get; set;}
 
-        public CrearVehiculosModel(){
+        public ListarVehiculo(){
             this.repositorioVehiculos = new RepositorioVehiculos(new VehiculosTransporte.App.Persistencia.AppContext());
         }
 
@@ -22,6 +24,15 @@ namespace VehiculosTransporte.App.Frontend.Pages
         {
             vehiculos = repositorioVehiculos.GetVehiculos();
 
+        }
+
+        public PartialViewResult OnPostEdit(string VehiculoId)
+        {
+            return new PartialViewResult
+            {
+                ViewName = "EditarVehiculo",
+                ViewData = new ViewDataDictionary<Bus>(ViewData, repositorioVehiculos.GetVehiculo(Int32.Parse(VehiculoId)))
+            };
         }
     }
 }
