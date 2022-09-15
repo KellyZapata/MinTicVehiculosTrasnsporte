7800cd5e35cceabc793e9045186bd4a02b4e5c7e
Henrry Pulgarin <xteams.hp@gmail.com>
2022-09-15 11:54:37 -0500

    persona

---
+++ VehiculosTransporte.App.Frontend/Pages/Personas/EditarPersona.cshtml.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VehiculosTransporte.App.Persistencia;
+using VehiculosTransporte.App.Dominio;
+
+namespace VehiculosTransporte.App.Frontend.Pages
+{
+    public class EditarPersonaModel : PageModel
+    {
+        private readonly IRepositorioPersonas repositorioPersonas;
+        [BindProperty]
+        public Persona persona {get; set;}
+
+        public EditarPersonaModel()
+        {
+            this.repositorioPersonas = new RepositorioPersonas(new VehiculosTransporte.App.Persistencia.AppContext());
+
+        }
+       public IActionResult OnGet(int? PersonaId)
+        {
+            if(PersonaId.HasValue)
+            {
+                persona = this.repositorioPersonas.GetPersona(PersonaId.Value);
+            }
+            else
+            {
+                persona = new Persona();
+            }
+
+            if(persona == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+        public IActionResult OnPost()
+        {
+            if(persona.Id > 0)
+            {
+                persona = this.repositorioPersonas.UpdatePersona(persona);
+            }
+            else
+            {
+                this.repositorioPersonas.AddPersona(persona);
+            }
+            return RedirectToPage("./ListarPersona");
+        }
+
+        public IActionResult OnGetDelete(string PersonaId)
+        {
+            this.repositorioPersonas.DeletePersona(int.Parse(PersonaId));
+            return RedirectToPage("./ListarPersona");
+        }
+    }
+}
